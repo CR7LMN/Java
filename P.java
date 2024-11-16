812edafcfcf0fb8ecf785b9038a25f7f00ba07fa
Farid <abdulkafimuhamad@gmail.com>
2024-11-16 12:49:16 +0700

    Add files via upload

---
+++ P.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) { 
+        // String dari kelas PanjangString
+        String firstName = "Cristiano Ronaldo";
+        String lastName = "Lionel Messi";
+        System.out.println("Panjang Nama Pertama : " + firstName.length());
+        System.out.println("Panjang Nama Kedua : " + lastName.length());
+    }
+}
---
+++ Test length.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        String firstName = "Cristiano Ronaldo ";
+        String lastName = "Lionel Messi";
+        System.out.println(firstName.length());
+        System.out.println(lastName.length());
+    }
+}
