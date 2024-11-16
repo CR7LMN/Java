3c035888dadce5558807d2155b2c1b03c89597d7
Farid <abdulkafimuhamad@gmail.com>
2024-11-16 12:59:06 +0700

    Delete P.java

--- P.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String[] args) { 
-        // String dari kelas PanjangString
-        String firstName = "Cristiano Ronaldo";
-        String lastName = "Lionel Messi";
-        System.out.println("Panjang Nama Pertama : " + firstName.length());
-        System.out.println("Panjang Nama Kedua : " + lastName.length());
-    }
-}
